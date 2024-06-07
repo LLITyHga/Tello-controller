e19e3c78f3650383c412ca746e24b6504cdc8680
Андрій Пікусь <llityhga@gmail.com>
2024-06-07 08:52:10 +0300

    Initial Commit

---
+++ Tello Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tello Controller
+//
+//  Created by Wolf on 07.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
