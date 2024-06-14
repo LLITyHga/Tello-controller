1aad1f0e5de90e3643e1d0bae33223488c871d19
Андрій Пікусь <llityhga@gmail.com>
2024-06-14 09:33:39 +0300

    рішення знайдено

--- Tello Controller/AppDelegate.swift
+++ Tello Controller/AppDelegate.swift
@@ -15,7 +15,7 @@ var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = ViewController()
+        window.rootViewController = MainViewController()
         window.makeKeyAndVisible()
         self.window = window
         return true
@@ -33,6 +33,10 @@ var window: UIWindow?
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .landscape
     }
 
 
